17a5e60d8013fae9704c636c5ee3bb07e4ddd973
Vitor Cordiolli <v.cordiolli@gmail.com>
2018-11-14 17:18:12 -0200

    atualização dos modelos
    fa, banda rating bla bla bla

--- app/models/fa.rb
+++ app/models/fa.rb
@@ -1,2 +1,4 @@
 class Fa < ApplicationRecord
+	# um fã pode ter vários ratings
+	has_many :ratings, through: :banda
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,7 @@
 class Rating < ApplicationRecord
+	# todo rating id é linkado a um fã, pelo fa_id
+	belongs_to :fa
+	# todo rating id é linkado a uma banda, pelo banda_id
+	belongs_to :banda
+	
 end
