f7214f8c08cf253058f0756ad7eac6108ca9f937
Daniel Gieseler <danielsgieseler@gmail.com>
2018-11-14 17:48:42 -0200

    agora vai

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,4 @@
 class Rating < ApplicationRecord
-	# todo rating id é linkado a um fã, pelo fa_id
-	belongs_to :fa
-	# todo rating id é linkado a uma banda, pelo banda_id
 	belongs_to :banda
+	belongs_to :fa
 end
