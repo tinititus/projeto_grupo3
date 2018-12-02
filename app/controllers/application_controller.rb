daeffd9a7df3f02eed5fc6fdd67589d2d266881b
Vitor C. Silvestre <v.cordiolli@gmail.com>
2018-12-02 13:02:57 -0200

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,10 @@ class ApplicationController < ActionController::Base
 	unless logged_in?
 		redirect_to root_url
 	end
+  end	
   def correct_usuario?
 	@usuario = Usuario.find_by(params[:id])
-		unless current_usuario == @usuario
-			redirect_to usuarios_path
-end
+	unless current_usuario == @usuario
+		redirect_to usuarios_path
+	end
+  end		
